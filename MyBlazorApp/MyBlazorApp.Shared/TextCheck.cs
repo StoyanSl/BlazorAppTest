e8238589a41968f2cd66ae4a1d468a7aaa7a19c7
StoyanSl <Stoyansl29@gmail.com>
2019-01-13 23:23:01 +0200

    Initial Commit

---
+++ MyBlazorApp/MyBlazorApp.Shared/TextCheck.cs
@@ -0,0 +1,11 @@
+﻿namespace MyBlazorApp.Shared
+{
+    public class TextCheck
+    {
+       public int RowId { get; set; }
+
+        public string OriginalRowText { get; set; }
+
+        public string ChangedRowText { get; set; }
+    }
+}
